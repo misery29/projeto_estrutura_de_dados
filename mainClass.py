80157e558bdae0df0efac09c8d7e68d186f069fe
brennoviana <brennovianacoelho@gmail.com>
2023-05-22 19:23:11 -0300

    Primeiro commit

---
+++ mainClass.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, tipo, idade, cor, porte, particularidade):
+        self.tipo = tipo
+        self.idade = idade
+        self.cor = cor
+        self.porte = porte
+        self.particularidade = particularidade
+
+class Pessoa:
+    def __init__(self, nome, contato, especie_interesse, preferencia):
+        self.nome = nome
+        self.contato = contato
+        self.especie_interesse = especie_interesse
+        self.preferencia = preferencia
+
