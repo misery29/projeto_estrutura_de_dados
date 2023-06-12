9f30fb6c68a115464b1551328feebd5df178b01f
João Pedro <117862733+misery29@users.noreply.github.com>
2023-06-12 20:31:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from mainClass import *
 from mainDefs import *
 
-# Função para executar os testes
-def testes():
-    sistema = Sistema()
+sistema = Sistema()
+
 
     # Cenário 1: Cadastrar animais e pessoas
-    animal1 = Animal("Canino", 3, "Marrom", "Pequeno", "Late muito")
-    animal2 = Animal("Felino", 5, "Preto", "Médio", "Gosta de brincar")
-    sistema.adiciona_animal(animal1)
-    sistema.adiciona_animal(animal2)
+animal1 = Animal("Felino", 3, "Rajado", "Pequeno", "Mija demais")
+animal2 = Animal("Canino", 8, "Branci", "Grande", "Late pouco")
+animal3 = Animal("Canino", 3, "Marrom", "Pequeno", "Late muito")
+animal4 = Animal("Felino", 5, "Preto", "Médio", "Gosta de brincar")
+sistema.adiciona_animal(animal1)
+sistema.adiciona_animal(animal2)
+sistema.adiciona_animal(animal3)
+sistema.adiciona_animal(animal4)
 
-    pessoa1 = Pessoa("João", "123456789", "Felino", "Gosta de animais brincalhões")
-    pessoa2 = Pessoa("Maria", "987654321", "Canino", "Prefere animais de pequeno porte")
-    sistema.adiciona_pessoa(pessoa1)
-    sistema.adiciona_pessoa(pessoa2)
 
-testes()
+pessoa1 = Pessoa("João", "123456789", "Felino", "Gosta de animais brincalhões")
+pessoa2 = Pessoa("Maria", "987654321", "Canino", "Prefere animais de pequeno porte")
+sistema.adiciona_pessoa(pessoa1)
+sistema.adiciona_pessoa(pessoa2)
+
+
+
+sistema.relatorio_cruzamento()
